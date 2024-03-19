5ec8874cb7c8a62182b045ef059f62fe67540f26
GiulioBarkee <giuliobarchi1@gmail.com>
2024-03-19 17:42:09 +0100

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ export default defineConfig({
       // Esempio: simuliamo una richiesta HTTP per recuperare i dati dal CMS
       const response = await fetch('https://paulinho.pages.dev/admin/#/collections/projects');
       const data = await response.json();
-      
       // Restituisci i dati ottenuti dal CMS
       return data;
     }
